8cabbc87bdc27ae72075ce7b64bbe1d4d8a7f2ca
Brian M <brian.meagher@ixsystems.com>
2025-09-25 08:50:00 -0700

    Upgrade SPDK from v24.09.x to v25.05.x
    Also set DEB_PYTHON_INSTALL_LAYOUT

--- fetch.sh
+++ fetch.sh
@@ -1,5 +1,5 @@
 #!/bin/sh -ex
-BRANCH=v24.09.x
+BRANCH=v25.05.x
 git clone -b $BRANCH https://github.com/spdk/spdk --recursive spdk.src
 rsync -rv --exclude=.git spdk.src/* .
 rm -rf spdk.src fetch.sh
