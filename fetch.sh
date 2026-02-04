4155591ef74c73807e72261187bfe86dc017b35c
Brian M <brian.meagher@ixsystems.com>
2026-02-04 08:19:35 -0800

    Upgrade SPDK from v25.05 to v26.01

--- fetch.sh
+++ fetch.sh
@@ -1,5 +1,11 @@
 #!/bin/sh -ex
-BRANCH=v25.05.x
+# Now need uv to install the python package
+curl -LsSf https://astral.sh/uv/0.9.29/install.sh | sh
+BRANCH=v26.01.x
+# Version v26.01
+# https://github.com/spdk/spdk/commit/2ef883ef96e79c3cc16da02f667a7a58c2453f2f
+COMMIT=2ef883ef96e79c3cc16da02f667a7a58c2453f2f
 git clone -b $BRANCH https://github.com/spdk/spdk --recursive spdk.src
+( cd spdk.src ; git checkout $COMMIT )
 rsync -rv --exclude=.git spdk.src/* .
 rm -rf spdk.src fetch.sh
